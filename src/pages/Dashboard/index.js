a32bec08ce9619c6dfd51e0f70ff3f6cf70db6ea
witalomelo <witalovmelo@gmail.com>
2024-03-29 16:57:08 -0300

    pagina de cadastro

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard() {
+    return(
+        <div>
+            <h1>dashboard</h1>
+        </div>
+    )
+}
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "../SignIn/signin.css";
+
+import logo from "../../assets/logo.png";
+
 export default function SignUp() {
-    return(
-        <div>
-            <h1>Sing Up</h1>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
         </div>
-    )
+
+        <form>
+          <h1>Cadastrar nova conta</h1>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Senha"
+            value={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já possui uma conta? Faça login</Link>
+      </div>
+    </div>
+  );
 }
